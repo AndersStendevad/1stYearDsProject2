cc7435541cf217599711d567bf75ef6e95ff21bc
Anders <andst@itu.dk>
2019-03-12 13:41:54 +0100

    This is a test

--- EDA.py
+++ EDA.py
@@ -1,6 +1,10 @@
 import backboning as bb
 
 filename = "Data/ydata-ysm-advertiser-phrase-adjlist.txt"
-column_of_interest = "MyDick"
 
-bb.read(filename, column_of_interest)
+column_of_interest = ["MyDick","MA"]
+
+Data = bb.read(filename, column_of_interest)
+
+def doDathing(data):
+    Profit
