7e054ff85bf219d60d042c1571c0f25f76929988
Anders <andst@itu.dk>
2019-03-14 14:34:47 +0100

    changed funtionallity so it can start with a command instead of having to manipulate the code

--- main.py
+++ main.py
@@ -4,14 +4,24 @@ import projetionStep as ps
 import discoveryStep as ds
 
 
-
 def main():
+    argument = input("write ps , bbs, ds, all")
+    if argument == "all":
 
-    ps.projetionStep(freshStart = True)
+        ps.projetionStep(freshStart = True)
 
-    bbs.backboningStep()
+        bbs.backboningStep()
 
-    ds.discoveryStep()
+        ds.discoveryStep()
+
+    elif argument == "ps":
+        ps.projetionStep(freshStart = True)
+
+    elif argument == "bbs":
+        bbs.backboningStep()
+
+    elif argument == "ds":
+        ds.discoveryStep()
 
 
     #profit("?")
