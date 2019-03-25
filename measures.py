8fad62614ed46ea6a7ef4a97afc845bd58b4eeb8
Edi Begovic <edibegovic@gmail.com>
2019-03-25 12:51:16 +0100

    Minor additions

--- measures.py
+++ measures.py
@@ -22,20 +22,31 @@ class Measures:
     # of customers (that is, a community)
     #
     # Format: (query, count) ///  e.g. (54555, 27)
-    def get_query_counts(self, customers):
+    def get_query_counts(self, customers, sort=True):
         query_dict = {}
     
         for query in self.get_queries(customers):
             query_dict[query] = query_dict.get(query, 0) + 1
 
-        return sorted(query_dict.items(), key=lambda x: x[1])[::-1]
+        if sort:
+            return sorted(query_dict.items(), key=lambda x: x[1])[::-1]
+        else:
+            return query_dict
 
-    # Returns a sorted list (of tuples) of all queries and the share of customers who bit on 
-    # that query in a given community.
+    # Returns the share of customers in a given community who bid on queries 
+    # of that comminuty. Sorted. 
     #
     # Format: (query, share%) /// e.g. (54555, 0.27)
     def get_query_shares(self, customers):
         return [(x[0],(x[1]/len(customers)*100)) for x in get_query_counts(customers)]
+
+    # Returns a list of all queries in a community and thier respective
+    # usage in the given community relative to its usage across all comminities. 
+    def get_query_representation(self, community, communities):
+        query_counts = get_query_counts(community)[:10]
+
+        for query_count in query_counts:
+            count = [x[1] for x in query_count][0]
 
     # -------- helpers --------
 
